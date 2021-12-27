9c866ecb84b4fabe88c85fa939f5c62c6ac3f3e8
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 17:45:13 +0200

    Add the home index view.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+        'products': Product.objects.all(),
+    }
+    return render(request, 'store/product_list.html', context)
