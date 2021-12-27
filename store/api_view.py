daaae8bba362a988110cede681ecbec1c0bbb2f3
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 17:33:36 +0200

    Reformat file using pep8.

--- store/api_view.py
+++ store/api_view.py
@@ -1,8 +1,7 @@
-from django.db.models.query import QuerySet
-from rest_framework import serializers
 from rest_framework.generics import ListAPIView
 from .serializers import ProductSerializer
 from .models import Product
+
 
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
