91af1e14e0542a2f42a8cf3d51ad82e139de81c3
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 18:09:37 +0200

    Overrride the getqueryset method.

--- store/api_views.py
+++ store/api_views.py
@@ -1,4 +1,6 @@
+from django.db.models import query
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ProductSerializer
 from .models import Product
 
@@ -6,3 +8,18 @@ from .models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('id',)
+
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sell', None)
+        if on_sale is None:
+            return super().get_queryset()
+        queryset = Product.objects.all()
+        if on_sale.lower() == 'true':
+            from django.utils import timezone
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now
+            )
