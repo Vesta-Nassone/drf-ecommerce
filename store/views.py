aee8d3e1b53a1b12fa4fac6bbfe306bfb42d700f
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 17:46:17 +0200

    Implement the cart view.

--- store/views.py
+++ store/views.py
@@ -16,3 +16,13 @@ def show(request, id):
         'product': Product.objects.get(id=id),
     }
     return render(request, 'store/product.html', context)
+
+def cart(request):
+    context = {
+        'items': [],
+        'subtotal': 1.0,
+        'tax_rate': int(ShoppingCart.TAX_RATE * 100.0),
+        'tax_total': 2.0,
+        'total': 3.0,
+    }
+    return render(request, 'store/cart.html', context)
