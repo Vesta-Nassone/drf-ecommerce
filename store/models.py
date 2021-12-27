eed4ae858a88b163d0db571722ad69aa57532583
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 16:53:09 +0200

    Implement the Product Model.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Product(models.Model):
+    DISCOUNT_RATE = 0.10
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    sale_start = models.DateTimeField(blank=True, null=True, default=None)
+    sale_end = models.DateTimeField(blank=True, null=True, default=None)
+    photo = models.ImageField(blank=True, null=True, default=None, upload_to='products')
+
+    
