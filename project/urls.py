067b0a19436abd1c20f8f7a197462ae043618df4
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 17:37:26 +0200

    Add static file config urls.

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 import store.views
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('products/<int:id>/', store.views.show, name='show-product'),
     path('cart/', store.views.cart, name='shopping-cart'),
     path('', store.views.index, name='list-products'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
