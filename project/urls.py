85716ecfee11a0a3a62444bbfb0b4c121d9e9b0b
Vesta-Nassone <vestanassone@gmail.com>
2021-12-27 17:36:25 +0200

    Add some urls paths.

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import store.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/<int:id>/', store.views.show, name='show-product'),
+    path('cart/', store.views.cart, name='shopping-cart'),
+    path('', store.views.index, name='list-products'),
 ]
