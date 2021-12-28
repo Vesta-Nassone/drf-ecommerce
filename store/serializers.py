ddee41faeefc9531eecb084a670600478b5ef402
Vesta-Nassone <vestanassone@gmail.com>
2021-12-28 15:46:34 +0200

    Refactor how the custom fields appear in the product serializer.

--- store/serializers.py
+++ store/serializers.py
@@ -5,7 +5,9 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     is_on_sale = serializers.BooleanField(read_only=True)
     current_price = serializers.FloatField(read_only=True)
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description',
-        'price', 'sale_start', 'sale_end')
+        fields = (
+            'id', 'name', 'description', 'price', 'sale_start', 'sale_end', 'is_on_sale', 'current_price',
+        )
