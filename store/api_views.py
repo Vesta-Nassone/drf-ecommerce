46723106b6b60ad47217319858ddb024a49b7570
Vesta-Nassone <vestanassone@gmail.com>
2021-12-28 14:45:04 +0200

    Implements the LimitOffsetPagination class.

--- store/api_views.py
+++ store/api_views.py
@@ -1,16 +1,22 @@
+from rest_framework import pagination
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from .serializers import ProductSerializer
 from .models import Product
 
 
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
     search_fields = ('name', 'description')
+    pagination_class = ProductsPagination
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sell', None)
