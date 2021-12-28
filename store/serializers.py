de42044d7dd75081816e5373d6703a83fc7d0cd1
Vesta-Nassone <vestanassone@gmail.com>
2021-12-28 15:44:34 +0200

    Delete the to_representation overridden method.

--- store/serializers.py
+++ store/serializers.py
@@ -3,14 +3,9 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    is_on_sale = serializers.BooleanField(read_only=True)
+    current_price = serializers.FloatField(read_only=True)
     class Meta:
         model = Product
         fields = ('id', 'name', 'description',
         'price', 'sale_start', 'sale_end')
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['is_on_sale'] = instance.is_on_sale()
-        data['current_price'] = instance.current_price()
-
-        return data
