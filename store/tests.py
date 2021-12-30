b0a541cb32d0a6709c7c310ed311c927f1644d83
Vesta-Nassone <vestanassone@gmail.com>
2021-12-30 20:47:33 +0200

    Create ProductTestCase from rest APITestCase.

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Product
 
 # Create your tests here.
+class ProductCreateTestCase(APITestCase):
+    
