3e5ae389d6fd5127a0765def2bf983f4c49bf1f4
Vesta-Nassone <vestanassone@gmail.com>
2021-12-30 21:26:49 +0200

    Implement the test_case_product method.

--- store/tests.py
+++ store/tests.py
@@ -7,5 +7,20 @@ from .models import Product
 class ProductCreateTestCase(APITestCase):
     def test_create_product(self):
         initial_product_count = Product.objects.count()
-        
+        product_attr = {
+            'name': 'New Product',
+            'description': 'Awesome Product',
+            'price': '120.50'
+        }
+        response = self.client.post('/api/v1/products/new', product_attr)
+        if response.status_code != 201:
+            print(response.data)
+        self.assertEqual(Product.objects.count(),
+                         initial_product_count+1,)
+        for attr, expected_value in product_attr.items():
+            self.assertEqual(response.data[attr], expected_value)
+        self.assertEqual(
+            response.data['current_price'],
+            float(product_attr['price'])
+        )
 
