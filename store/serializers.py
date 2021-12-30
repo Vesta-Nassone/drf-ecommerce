1ce4980d3ae98f0314e435d22281fa78fbbb0fd1
Vesta-Nassone <vestanassone@gmail.com>
2021-12-30 21:32:51 +0200

    Set the required attribute for DateTimeField to False.

--- store/serializers.py
+++ store/serializers.py
@@ -12,12 +12,14 @@ class ProductSerializer(serializers.ModelSerializer):
     price = serializers.DecimalField(
         min_value=1, max_value=100000, max_digits=None, decimal_places=2)
     sale_start = serializers.DateTimeField(
+        required=False,
         input_formats=['%I:%M % p % d % B % Y'],
         format=None, allow_null=True,
         help_text='Accepted format is "12:01 PM 16 December 2021"',
         style={'input_type': 'text', 'placeholder': '12:01 AM 28 July 2019'}
     )
     sale_end = serializers.DateTimeField(
+        required=False,
         input_formats=['%I:%M % p % d % B % Y'],
         format=None, allow_null=True,
         help_text='Accepted format is "12:01 PM 16 December 2021"',
