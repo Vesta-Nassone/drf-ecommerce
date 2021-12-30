c85e3d60c1cac12700e44a082fc66f635eb34484
Vesta-Nassone <vestanassone@gmail.com>
2021-12-30 21:26:28 +0200

    Define the test_case_product method.

--- store/tests.py
+++ store/tests.py
@@ -2,5 +2,10 @@ from rest_framework.test import APITestCase
 from .models import Product
 
 # Create your tests here.
+
+
 class ProductCreateTestCase(APITestCase):
-    
+    def test_create_product(self):
+        initial_product_count = Product.objects.count()
+        
+
